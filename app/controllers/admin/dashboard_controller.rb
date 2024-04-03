ab3de10b58c98b0d9fa48cab85b59cf5d7ece925
v-lane <victoria.lane.m@gmail.com>
2024-04-03 17:44:58 -0400

    Add controller data fetch

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
   end
 end
