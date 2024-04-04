89c6f4d013c5b77311652ab588b36da6262593ee
v-lane <victoria.lane.m@gmail.com>
2024-04-04 08:57:14 -0400

    Add route methods

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,13 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
