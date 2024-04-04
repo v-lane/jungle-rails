5490a08739b92787f6479aed4de037470fecbdcb
v-lane <victoria.lane.m@gmail.com>
2024-04-04 09:19:24 -0400

    Add data

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def index
+    @categories = Category.order(name: :asc).all
   end
 
   def new
