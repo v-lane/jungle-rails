5706561db6f213b8cf3136f5a79e6d6dd268efab
v-lane <victoria.lane.m@gmail.com>
2024-04-04 12:23:01 -0400

    Add register and login views formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else 
-      redirect_to new_user_path
+      redirect_to register_path
     end
   end
 
