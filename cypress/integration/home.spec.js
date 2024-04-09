308fa711d6fba9549a1bc8af5665725f973c280c
v-lane <victoria.lane.m@gmail.com>
2024-04-09 12:02:34 -0400

    Add Cypress home page tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('Home Page', () => {
+
+  it('should visit root', () => {
+    cy.visit('/')
+  })
+
+  it("There are products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
