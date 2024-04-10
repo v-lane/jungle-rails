441c87dbc1c5ba3b38dc60071dfce7b956b66ed2
v-lane <victoria.lane.m@gmail.com>
2024-04-10 16:04:56 -0400

    Add order confirmation email text

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,6 @@ class OrderMailer < ApplicationMailer
 
   def order_confirmation_email
     @order = params[:order]
-    @url = ''
     mail(to: @order.email, subject: "Order confirmation: #{@order.id}")
   end
 end
