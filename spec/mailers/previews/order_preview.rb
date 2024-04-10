4f826df54ec25faeabbbeb74378fd0ebbac09554
v-lane <victoria.lane.m@gmail.com>
2024-04-10 17:05:00 -0400

    Fix bugs

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order
 class OrderPreview < ActionMailer::Preview
-
+  def order_confirmation_email
+    OrderMailer.with(order: Order.first).order_confirmation_email
+  end
 end
